d53e2feca339080ce176f69d919e67234d62aebd
José Alexandre <jafidelis@gmail.com>
2018-08-07 21:54:17 -0300

    Adicionado tecla de atalho

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -19,17 +19,24 @@ export default class Todo extends Component {
         this.handleRemove = this.handleRemove.bind(this);
         this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
         this.handleMarkAsPeding = this.handleMarkAsPeding.bind(this);
+        this.handleSearch = this.handleSearch.bind(this);
+        this.handleClear = this.handleClear.bind(this);
         this.refresh();
     }
 
-    refresh() {
-        axios.get(`${URL}?sort=-createdAt`).then(resp => 
+    refresh(description = '') {
+        const search = description ? `&description__regex=/${description}/` : '';
+        axios.get(`${URL}?sort=-createdAt${search}`).then(resp => 
             this.setState({
                 ...this.state, 
-                description: '',
+                description,
                 list: resp.data
             })
         );
+    }
+
+    handleSearch() {
+        this.refresh(this.state.description);
     }
 
     handleChange(e) {
@@ -42,15 +49,19 @@ export default class Todo extends Component {
     }
 
     handleRemove(todo) {
-        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh());
+        axios.delete(`${URL}/${todo._id}`).then(resp => this.refresh(this.state.description));
     }
 
     handleMarkAsDone(todo) {
-        axios.put(`${URL}/${todo._id}`, { ...todo, done: true}).then(resp => this.refresh());
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: true}).then(resp => this.refresh(this.state.description));
     }
 
     handleMarkAsPeding(todo) {
-        axios.put(`${URL}/${todo._id}`, { ...todo, done: false}).then(resp => this.refresh());
+        axios.put(`${URL}/${todo._id}`, { ...todo, done: false}).then(resp => this.refresh(this.state.description));
+    }
+
+    handleClear() {
+        this.refresh();
     }
 
     render() {
@@ -60,7 +71,9 @@ export default class Todo extends Component {
                 <TodoForm 
                     handleAdd={this.handleAdd} 
                     description={this.state.description} 
-                    handleChange={this.handleChange} />
+                    handleChange={this.handleChange}
+                    handleSearch={this.handleSearch}
+                    handleClear={this.handleClear} />
                 <TodoList 
                     list={this.state.list} 
                     handleRemove={this.handleRemove}
