8516a04db69b4015050c3f61454bb66c272ba75e
José Alexandre Fidelis <jose.alexandre@triersistemas.com.br>
2018-08-14 14:32:09 -0300

    Finalizado aplicativo

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,22 +1,9 @@
 import { combineReducers } from 'redux';
+import todoReducer from '../todo/todoReducer';
+
 
 const rootReducer = combineReducers({
-    todo: () => ({
-        description: 'Ler o Livro',
-        list: [{
-            _id: 1,
-            description: 'pagar fatura cartão',
-            done: true
-        }, {
-            _id: 2,
-            description: 'Reunião com equipe às 10',
-            done: false
-        }, {
-            _id: 3,
-            description: 'Consulta médica na terça depois do almoço',
-            done: false
-        }]
-    })
+    todo: todoReducer
 });
 
 export default rootReducer;
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import IconButton from '../template/iconButton';
+import { bindActionCreators } from 'redux';
+import { markAsDone, markAsPending, remove } from './todoActions';
 
 const TodoList = props => {
 
@@ -10,9 +12,9 @@ const TodoList = props => {
             <tr key={todo._id}>
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
-                    <IconButton style='success' icon='check' onClick={() => props.handleMarkAsDone(todo)} hide={todo.done}></IconButton>
-                    <IconButton style='warning' icon='undo' onClick={() => props.handleMarkAsPeding(todo)} hide={!todo.done}></IconButton>
-                    <IconButton style='danger' icon='trash-o' onClick={() => props.handleRemove(todo)} hide={!todo.done}></IconButton>
+                    <IconButton style='success' icon='check' onClick={() => props.markAsDone(todo)} hide={todo.done}></IconButton>
+                    <IconButton style='warning' icon='undo' onClick={() => props.markAsPending(todo)} hide={!todo.done}></IconButton>
+                    <IconButton style='danger' icon='trash-o' onClick={() => props.remove(todo)} hide={!todo.done}></IconButton>
                 </td>
             </tr>
         ))
@@ -34,5 +36,6 @@ const TodoList = props => {
 }
 
 const mapStateToProps = state => ({ list: state.todo.list });
+const mapDispatchToProps = dispatch => bindActionCreators({ markAsDone, markAsPending, remove }, dispatch);
 
-export default connect(mapStateToProps)(TodoList);
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList);
